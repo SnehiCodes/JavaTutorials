2410067aa489793f72bfe825b69aa3c3e4802d43
SnehiCodes <111358189+SnehiCodes@users.noreply.github.com>
2022-08-27 21:51:01 +0530

    Add files via upload

---
+++ java tutorials/Method Chaining/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String [] args){
+        // Method Chaining = a common syntax for invoking multiple method calls in OOP
+        // .                 condense code into less lines
+
+
+        String name = "       sneHI";
+
+        //name = name.concat("    code   ");
+        //name = name.toUpperCase();
+        //name = name.trim();
+
+        name = name.concat("                David     ").toUpperCase().trim();
+
+        System.out.println(name);
+
+
+
+
+
+
+    }
+
+}
