84f8fd2bc4ae5397668a7803e67af9764ef3fe1d
Snehi <111358189+SnehiCodes@users.noreply.github.com>
2022-09-25 22:55:40 +0530

    Add files via upload

---
+++ Custom Exceptions/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+
+        // exception = an event , after execution , that disrupts the normal flow of the program
+        // ex. Arithmetic Exception , ArrayIndexOutOfBoundsException
+
+    }
+}
