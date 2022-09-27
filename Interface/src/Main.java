271b757fd6cc43d19b0bee4d888c28326b64cc39
Snehi <111358189+SnehiCodes@users.noreply.github.com>
2022-09-27 17:20:29 +0530

    Add files via upload

---
+++ Interface/src/Main.java
@@ -0,0 +1,22 @@
+public class Main{
+    public static void main(String[] args){
+
+        // interface = a template that can be applied to a class.
+        //             similar to inheritance, but specifies what a class has/must do.
+        // .            classes can apply more than one interface , inheritance is limited to 1 super
+
+            Rabbit rabbit = new Rabbit();
+
+            rabbit.flee();
+
+            Hawk hawk = new Hawk();
+            hawk.hunt();
+            //hawk.flee();
+
+            Fish fish = new Fish();
+            fish.hunt();
+            fish.flee();
+
+
+    }
+}
---
+++ Interface/src/Rabbit.java
@@ -0,0 +1,7 @@
+public class Rabbit implements Prey{
+
+    public void flee(){
+        System.out.println("*The rabbit is fleeing.*");
+    }
+}
+
