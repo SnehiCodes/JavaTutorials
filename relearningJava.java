6658d0ea912cd4b1d2684ae7419d709af47fe092
Snehi <111358189+SnehiCodes@users.noreply.github.com>
2022-09-28 21:57:43 +0530

    Add files via upload

---
+++ relearningJava.java
@@ -0,0 +1,15 @@
+class Main{
+    public static void main(String[] args){
+        System.out.println(" \tCoconut-butter\n");
+        System.out.println("\\I \"love\" Myself");
+
+        int x=122;
+        long y= 2343456567878989L;
+        System.out.println(x)+ 4);
+// comment
+
+
+    }
+
+
+}
