03d1f2996dc839759277a0533d45a28a404d9491
Snehi <111358189+SnehiCodes@users.noreply.github.com>
2023-01-23 20:39:40 +0530

    Add files via upload

---
+++ JavaGUI/src/Main.java
@@ -0,0 +1,35 @@
+import javax.swing.JFrame;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args){
+        /*
+        * // JFrame frame = a GUI window to add components to
+
+        JFrame frame = new JFrame(); //creates a frame
+
+        frame.setTitle("JFrame title goes there");// sets title of frame
+
+        //frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setResizable(false);
+
+        frame.setSize(420,470);// sets the x-direction, and y-dimension of frame
+
+        frame.setVisible(true); // make frame visible
+
+        //ImageIcon image = new ImageIcon("\"C:\\Users\\snehi\\Pictures\\snehi.jpg\""); // create an image icon
+        //frame.setIconImage(image.getImage()); // chane icon of frame
+
+        //frame.getContentPane().setBackground(Color.ORANGE);
+        frame.getContentPane().setBackground(new Color(234,67,90));
+
+        *
+        * */
+
+        MyFrame myFrame = new MyFrame();
+    }
+}
