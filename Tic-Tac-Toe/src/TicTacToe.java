b573ce5403b34bb758dee3af308a6cda42510ea4
Snehi <111358189+SnehiCodes@users.noreply.github.com>
2023-02-06 00:10:07 +0530

    Add files via upload

---
+++ Tic-Tac-Toe/src/TicTacToe.java
@@ -0,0 +1,31 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.swing.*;
+
+public class TicTacToe implements ActionListener{
+
+    Random random = new Random();
+    JFrame frame = new Jframe();
+    JPanel
+    TicTacToe(){
+
+    }
+
+    public static actionPerformed(ActionEvent e){
+
+    }
+
+    public void firstTurn(){
+
+    }
+    public void check(){
+
+    }
+    public void xWins(int a,int b,int c){
+
+    }
+    public void oWins(int a,int b,int c){
+
+    }
+}
